41f6926fddc8a059d419478e4d4cb2fd276fbf16
Sorravit Bunjongpean <sorravit16758@gmail.com>
2016-11-05 15:38:00 +0700

    Change dependency and query

--- src/main/java/com/webboard/models/PostDAO.java
+++ src/main/java/com/webboard/models/PostDAO.java
@@ -1,9 +1,12 @@
 package com.webboard.models;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.annotations.Table;
+import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,16 +20,25 @@ import java.util.List;
 
 @Transactional
 public interface PostDAO extends CrudRepository<PostVO,Integer> {
-    @Query(value = "select p from PostVO p where p.idPost between :start AND :end ")
-    List<PostVO> findInIdRange(@Param("start") int start, @Param("end") int end); //{
-//        Query q = session.createQuery("select p from PostVO");
-//        q.setFirstResult(start);
-//        q.setMaxResults(end);
-//        return q.list();
-//    }
+//    @Query(value = "select p from PostVO p limit :start , :end ")
+    default  List<PostVO> findInIdRange(@Param("start") int start, @Param("end") int end) {
+        SessionFactory sessionFactory = new Configuration().configure().
+                buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Query q = session.createQuery("select p from PostVO");
+        q.setFirstResult(start);
+        q.setMaxResults(end);
+        return q.list();
+    }
 
     @Modifying
-    @Query(value = "update PostVO p set view_count = view_count +1 where  id_post = :id")
-    int updateViewCount(@Param("id") int id);
+//    @Query(value = "update PostVO p set view_count = view_count +1 where  id_post = :id")
+    default int updateViewCount(@Param("id") int id){
+        SessionFactory sessionFactory = new Configuration().configure().
+                buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Query q = session.createQuery("update PostVO p set view_count = view_count +1 where  id_post = :id");
+        return id;
+    }
 
 }
